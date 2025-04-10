2f094a00c3d931fb504ed593eacd49a421c44be3
Julien Marusi <julien.marusi@bbee.fr>
2025-04-10 11:47:45 +0200

    fix ip

--- src/config/entrypoint.js
+++ src/config/entrypoint.js
@@ -1,3 +1,5 @@
-export const ENTRYPOINT = process.env.REACT_APP_ENTRYPOINT
+// export const ENTRYPOINT = process.env.REACT_APP_ENTRYPOINT
+const host = window.location.hostname
+export const ENTRYPOINT = `http://${host}:4444/api/v1`
 export const THUMB = process.env.REACT_APP_THUMB
 export const IMAGE = process.env.REACT_APP_IMAGE
