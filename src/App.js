a3292be6e28effc4d5ca312fe27d467db75f8997
Julien Marusi <julien.marusi@bbee.fr>
2025-04-10 11:17:49 +0200

    check entrypoint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ENTRYPOINT } from '../config/entrypoint';
 import GalleryList from './components/GalleryList';
 import GalleryDetail from './components/GalleryDetail';
 import PhotoFullScreen from './components/PhotoFullScreen';
@@ -8,8 +9,8 @@ import Settings from './utils/settings';
 import './App.css';
 
 function App() {
-    const [settings, setSettings] = useState(false);
-  
+  const [settings, setSettings] = useState(false);
+
   // Function that calls loadSettings
   const initializeSettings = async () => {
     const ok = await Settings.loadSettings();
@@ -30,20 +31,21 @@ function App() {
   if (!settings) {
     return (
       <div
-      className="error-message"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        textAlign: 'center'
-      }}
+        className="error-message"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          textAlign: 'center'
+        }}
       >
-      <div style={{ fontSize: '100px' }}>❌</div>
-      <h2>Erreur d'initialisation de l'application</h2>
-      <p>- Vérifier que le serveur soit démarré</p>
-      <p>- Si vous utilisez PostgreSQL, vérifier le cluster</p>
+        <div style={{ fontSize: '100px' }}>❌</div>
+        <h2>Erreur d'initialisation de l'application</h2>
+        <p>- Vérifier que le serveur soit démarré</p>
+        <p>- Le serveur doit etre configuré sur {ENTRYPOINT}</p>
+        <p>- Si vous utilisez PostgreSQL, vérifier le cluster</p>
       </div>
     );
   } else return (
